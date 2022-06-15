ef0ac10bcbc5f58ecedd7d942daf631a2d0f106c
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-06-15 18:17:19 +0530

    complete footer and bottom page
    complete footer and bottom page

--- client/src/Components/Bottom.jsx
+++ client/src/Components/Bottom.jsx
@@ -1,54 +1,55 @@
 import React from 'react'
-import '../style/bottom.css'
-
+import style from '../style/bottom.module.css'
 
 const Bottom = () => {
     return (
         <div>
 
-            <div className="wrapper">
-                <div className="flex">
-                    <div className="left">
+            <div className={style.wrapper}>
+                <div className={style.flex}>
+                    <div className={style.left}>
                         <h1>Download the ModeSens App</h1>
                        <br /> <p>A seamless experience that takes your style to the next level</p>
-                        <a href="https://play.google.com/store/apps/details?id=com.modesens.androidapp&hl=en_IN&gl=US" target="_blank" rel="noreferrer">
+                        <a href="https://play.google.com/store/apps/details?id=com.modesens.androidapp&hl=en_IN&gl=US"
+                         target="_blank" rel="noreferrer">
                         {" "}
                         <br />
                         <br />
-                        <button className="btn">DOWNLOAD NOW</button>
+                        <button className={style.btn}>DOWNLOAD NOW</button>
                     </a>
                     <br />
                     <br />
-                    <div className="ImgDiv1">
+                    <div className={style.ImgDiv1}>
                         <img
                             src="https://cdn.modesens.com/static/img/20220420AppDownloaden.png"
                             alt="j"
-                           className='image_mobile'
+                           className={style.image_mobile}
                         />
                     </div>
                 </div>
-                <div className="right">
+                <div className={style.right}>
                     <h1>Install the ModeSens Browser Extension</h1>
                     <br /><p>Get timely, accurate product information every time you browse</p>
-                    <a href="https://addons.mozilla.org/en-GB/firefox/addon/modesens-shopping-assistant/" target="_blank" rel="noreferrer">
+                    <a href="https://addons.mozilla.org/en-GB/firefox/addon/modesens-shopping-assistant/"
+                     target="_blank" rel="noreferrer">
                     {" "}
                     <br />
                     <br />
-                    <button className="btn">INSTALL NOW</button>
+                    <button className={style.btn}>INSTALL NOW</button>
                 </a>
                 <br />
                 <br />
-                <div className="ImgDiv2">
+                <div className={style.ImgDiv2}>
                     <img
                         src="https://cdn.modesens.com/static/img/20211109downloadright.png"
                         alt="f"
-                        className='image_mobile'
+                        className={style.image_mobile}
                     />
                 </div>
             </div>
         </div>
     </div >
-    <p className='botton_p'>When you follow our links to visit a brand or retailer’s website or make a purchase, ModeSens may earn a commission.</p>
+    <p className={style.botton_p}>When you follow our links to visit a brand or retailer’s website or make a purchase, ModeSens may earn a commission.</p>
     </div >
   )
 }
