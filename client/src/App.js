08a54e56dfbb2346d863c16d17c1ae7d3929f368
Muskan Shaw <princesahil0065@gmail.com>
2022-06-15 16:00:42 +0530

    trending done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+import  SlideItem  from './Components/Trending'
+function App() {
+  return (
+    <div className="App">
+       <SlideItem/>
+    </div>
+  );
+}
+
+export default App;
