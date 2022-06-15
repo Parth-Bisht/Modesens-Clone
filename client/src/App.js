efa6b075d55276ace770f7cd214763c1f5da9c51
Muskan Shaw <princesahil0065@gmail.com>
2022-06-15 16:40:50 +0530

    login done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import CommunityPost from "./Components/Community"
-import { SignUp } from './Components/Signup';
+import { Login } from './Components/Login';
 import  Trending  from './Components/Trending'
 function App() {
   return (
     <div className="App">
        <Trending/>
        <CommunityPost/>
-       <SignUp/>
+       <Login/>
     </div>
   );
 }
--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,15 +1,9 @@
 import { React } from "react";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-import axios from "axios";
-import { CloseIcon } from "@chakra-ui/icons";
-import "./Login.css";
-import loginPic from "../images/login.png";
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import style from "../Styles/Login.module.css";
 
-export const SignUp = ({ isShowLogin }) => {
-  const { handleAuth } = useContext(AuthContext);
+export const Login = ({ isShowLogin }) => {
   const [formData, setFormData] = useState({});
 
   const handleChange = (e) => {
@@ -35,7 +29,7 @@ export const SignUp = ({ isShowLogin }) => {
           userArr.push(formData);
           console.log(userArr, "arrr");
           localStorage.setItem("userDatas", JSON.stringify(userArr));
-          handleAuth();
+        //   handleAuth();
         }
       } else {
         alert("enter a valid email");
@@ -47,21 +41,21 @@ export const SignUp = ({ isShowLogin }) => {
 
   return (
     <div className={`${isShowLogin ? "active" : "notActive"} show`}>
-      <div className="containerLogin">
-        <div className="left">
-          <img src={loginPic} alt="" />
+      <div className={style.containerLogin}>
+        <div className={style.left}>
+          <img src="https://modesens-web15.vercel.app/assets/login.2d1c8a59.png" alt="" />
         </div>
-        <div className="rightWrapper">
-          <div className="right">
+        <div className={style.rightWrapper}>
+          <div className={style.right}>
             <h2>Create an account</h2>
             <p>Compare across 500+ stores</p>
-            <p className="adjust">to find the best price.</p>
-            <form className=" formData " onSubmit={handleSubmit}>
+            <p className={style.adjust}>to find the best price.</p>
+            <form className={style.formData} onSubmit={handleSubmit}>
               <input
                 type="text"
                 id="email"
                 placeholder="Email"
-                className="email"
+                className={style.email}
                 value={formData.email}
                 onChange={handleChange}
               />
@@ -69,28 +63,28 @@ export const SignUp = ({ isShowLogin }) => {
                 type="password"
                 id="pass"
                 placeholder="Password"
-                className="password"
+                className={style.password}
                 value={formData.password}
                 onChange={handleChange}
               />
               <br />
-              <label className="radioBtn" htmlFor="radioA">
+              <label className={style.radioBtn} htmlFor="radioA">
                 <input type="radio" name="radio1" value="A" /> &nbsp; Subscribe
                 to personalized sale updates and offers
               </label>
               <br />
-              <input className="login" type="submit" value={"Sign Up"} />
+              <input className={style.login} type="submit" value={"Sign Up"} />
             </form>
-            <div className="orDiv">
-              <div className="firsthr">
+            <div className={style.orDiv}>
+              <div className={style.firsthr}>
                 <hr />
               </div>
-              <div className="or">or</div>
-              <div className="secondhr">
+              <div className={style.or}>or</div>
+              <div className={style.secondhr}>
                 <hr />
               </div>
             </div>
-            <div className="logos">
+            <div className={style.logos}>
               <div>
                 <img
                   src="https://modesens.com/static/img/login-icon/20210617google.svg"
@@ -116,10 +110,10 @@ export const SignUp = ({ isShowLogin }) => {
                 />
               </div>
             </div>
-            <a href="#" onClick={handleAuth}>
+            <a href="#" style={{textDecoration:"none",color:"black"}} >
               Already have an account? Please sign in.
             </a>
-            <div className="signinfooter">
+            <div className={style.signinfooter}>
               By creating an account, I agree to the{" "}
               <a href="##">Terms Of Use</a>
               and the <a href="##">Privacy Policy</a>
