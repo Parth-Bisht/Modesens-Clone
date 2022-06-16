eb7b5babe7e0fd4719dbeaa38b07b0c70c9e4f6e
Nandu Dumare <dumarenandu70@gmail.com>
2022-06-16 18:05:05 +0530

    design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Design from './Components/Design/Design';
 import MainSlider from './Components/Sliders/MainSlider';
 
 function App() {
   return (
     <div className="App">
-     <MainSlider/>
+      {/* <MainSlider/> */}
+      <Design/>
     </div>
   );
 }
