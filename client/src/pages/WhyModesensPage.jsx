6a6afb5e69adf1ce29aabd8d79b11f8835b41de5
Parth Bisht <bishtparth795@gmail.com>
2022-06-16 12:17:02 +0530

    whymod

---
+++ client/src/pages/WhyModesensPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WhyMod from '../components/WhyModesens/WhyMod'
+
+const WhyModesensPage = () => {
+  return (
+    <div>
+      <WhyMod/>
+    </div>
+  )
+}
+
+export default WhyModesensPage
